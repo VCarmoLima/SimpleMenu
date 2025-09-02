ba5483d90a17e5e4d2c6fc6f3026d94caf79bbca
VCarmoLima <viniciuscarmo.contato@gmail.com>
2025-09-01 23:25:44 -0300

    feat: Adiciona loop `do-while` e simulação de processamento ao menu
    O programa foi refatorado para operar dentro de um laço `do-while`, permitindo que o menu seja exibido repetidamente sem a necessidade de reiniciar a aplicação.
    
    Principais mudanças:
    - Uma nova opção "5. Repetir" foi adicionada para controlar o loop e exibir o menu novamente.
    - Foi implementado um `Thread.sleep(750)` em cada opção para simular um tempo de processamento, melhorando o feedback visual para o usuário.

--- Menu/src/Main.java
+++ Menu/src/Main.java
@@ -3,45 +3,83 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int op;
 
-        System.out.println("|       MENU      |");
-        System.out.println("| --------------- |");
-        System.out.println("| Opções:         |");
-        System.out.println("|    1. Opção 1   |");
-        System.out.println("|    2. Opção 2   |");
-        System.out.println("|    3. Opção 3   |");
-        System.out.println("|    4. Sair      |");
-        System.out.println("| --------------- |");
-        System.out.println(" ");
+        do {
+            System.out.println("|       MENU      |");
+            System.out.println("| --------------- |");
+            System.out.println("| Opções:         |");
+            System.out.println("|   1. Opção 1    |");
+            System.out.println("|   2. Opção 2    |");
+            System.out.println("|   3. Opção 3    |");
+            System.out.println("|    4. Sair      |");
+            System.out.println("|   5. Repetir    |");
+            System.out.println("| --------------- |");
+            System.out.println(" ");
 
-        System.out.print("Selecione uma opção: ");
-
-        while (!sc.hasNextInt()) {
-            System.out.println("Entrada inválida! Por favor, digite novamente.");
-            System.out.println(" --------------- ");
             System.out.print("Selecione uma opção: ");
-            sc.next();
-        }
 
-        int op = sc.nextInt();
+            while (!sc.hasNextInt()) {
+                System.out.println("Entrada inválida! Por favor, digite novamente.");
+                System.out.println(" --------------- ");
+                System.out.print("Selecione uma opção: ");
+                sc.next();
+            }
 
-        switch (op) {
-            case 1:
-                System.out.println("Opção 1 selecionada!");
-                break;
-            case 2:
-                System.out.println("Opção  2 selecionada!");
-                break;
-            case 3:
-                System.out.println("Opção 3 selecionada!");
-                break;
-            case 4:
-                System.out.println("Saindo...");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Seleção inválida!");
-        }
+            var process = "PROCESSANDO...";
+
+            op = sc.nextInt();
+
+            switch (op) {
+                case 1:
+                    System.out.println("Opção 1 selecionada!");
+                    System.out.println(process);
+                    try {
+                        Thread.sleep(750);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 2:
+                    System.out.println("Opção  2 selecionada!");
+                    System.out.println(process);
+                    try {
+                        Thread.sleep(750);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Opção 3 selecionada!");
+                    System.out.println(process);
+                    try {
+                        Thread.sleep(750);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 4:
+                    System.out.println("Saindo...");
+                    System.exit(0);
+                    try {
+                        Thread.sleep(750);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 5:
+                    System.out.println("Repetindo...");
+                    try {
+                        Thread.sleep(750);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    System.out.println("Seleção inválida!");
+            }
+
+        } while (op == 5);
 
         sc.close();
     }
